5740473920d6ebedb35a1a77bafa9cb89d81695a
dshu <dshu1213@163.com>
2020-09-02 15:02:08 +0800

    debug

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "YOLO",
   description: "dshu`s blog",
   theme: "yolo",
-  base: "/blog",
+  base: "/blog/",
   markdown: {
     anchor: { permalink: false },
     toc: { includeLevel: [1, 2, 3, 4, 5] },
