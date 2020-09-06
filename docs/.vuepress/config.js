262eb8325a112f006c104a5153610a98cae1bb93
dshuu <hudson.hutu@gmail.com>
2020-09-06 23:41:32 +0800

    jscore

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  title: "YOLO",
-  description: "dshu`s blog",
-  theme: "yolo",
-  base: "/blog/",
+  title: 'YOLO',
+  description: 'dshu`s blog',
+  theme: 'yolo',
+  base: '/blog/',
   markdown: {
     anchor: { permalink: false },
     toc: { includeLevel: [1, 2, 3, 4, 5] },
   },
-};
+}
